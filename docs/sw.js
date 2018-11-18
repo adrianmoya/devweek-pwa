c6e36e6cb56bef8f62081ca0c12a3a07040db05f
Adrian Moya <adrian.moya.consultant@sothebys.com>
2018-11-18 14:23:29 -0500

    Modified github pages version

--- docs/sw.js
+++ docs/sw.js
@@ -31,3 +31,11 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
+
+self.addEventListener('push', function(event) {
+  if (event.data) {
+    console.log('This push event has data: ', event.data.text());
+  } else {
+    console.log('This push event has no data.');
+  }
+});
